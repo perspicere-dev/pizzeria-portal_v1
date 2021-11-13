c9003047ba8cb3b0ba2a3277205289894213dd62
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-11-13 10:59:39 +0100

    add login and waiter views

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import styles from './Login.module.scss';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import {useNavigate} from 'react-router-dom';
+
+
 
 const Login = () => {
+  
+  const navigate = useNavigate();
+
   return (
     <div className={styles.component}>
-      <h2>Login view</h2>
+      <h2>Login</h2>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="Id" variant="outlined" />
+        <TextField id="filled-basic" label="Password" variant="outlined" />
+        <Button 
+          variant="outlined"
+          onClick={() => navigate('/', { replace: true })}
+        >LogIn
+        </Button>      
+      </Box>
     </div>
+    
   );
 };
 
--- src/components/views/Waiter/Waiter.js
+++ src/components/views/Waiter/Waiter.js
@@ -1,15 +1,91 @@
 import React from 'react';
 import styles from './Waiter.module.scss';
-import { Link } from 'react-router-dom';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
 
-const Waiter = () => {
-  return (
-    <div className={styles.component}>
-      <h2>Waiter view</h2>
-      <Link to={process.env.PUBLIC_URL +`/waiter/order/new`}>New order</Link>
-      <Link to={process.env.PUBLIC_URL +`/waiter/order/:id`}>Order</Link>
-    </div>
-  );
+const demoContent = [
+  {id: '1', status: 'free', order: null},
+  {id: '2', status: 'thinking', order: null},
+  {id: '3', status: 'ordered', order: 123},
+  {id: '4', status: 'prepared', order: 234},
+  {id: '5', status: 'delivered', order: 345},
+  {id: '6', status: 'paid', order: 456},
+];
+
+const renderActions = status => {
+  switch (status) {
+    case 'free':
+      return (
+        <>
+          <Button>thinking</Button>
+          <Button>new order</Button>
+        </>
+      );
+    case 'thinking':
+      return (
+        <Button>new order</Button>
+      );
+    case 'ordered':
+      return (
+        <Button>prepared</Button>
+      );
+    case 'prepared':
+      return (
+        <Button>delivered</Button>
+      );
+    case 'delivered':
+      return (
+        <Button>paid</Button>
+      );
+    case 'paid':
+      return (
+        <Button>free</Button>
+      );
+    default:
+      return null;
+  }
 };
+
+const Waiter = () => (
+  <Paper className={styles.component}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Table</TableCell>
+          <TableCell>Status</TableCell>
+          <TableCell>Order</TableCell>
+          <TableCell>Action</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {demoContent.map(row => (
+          <TableRow key={row.id}>
+            <TableCell component="th" scope="row">
+              {row.id}
+            </TableCell>
+            <TableCell>
+              {row.status}
+            </TableCell>
+            <TableCell>
+              {row.order && (
+                <Button to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>
+                  {row.order}
+                </Button>
+              )}
+            </TableCell>
+            <TableCell>
+              {renderActions(row.status)}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </Paper>
+);
 
 export default Waiter;
