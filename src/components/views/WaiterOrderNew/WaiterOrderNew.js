015b3a79c739bb15e1a20e285c845c522fb87abb
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-11-17 16:50:51 +0100

    add options checkboxes and radios for waiter

--- src/components/views/WaiterOrderNew/WaiterOrderNew.js
+++ src/components/views/WaiterOrderNew/WaiterOrderNew.js
@@ -19,7 +19,8 @@ import Radio from '@mui/material/Radio';
 // import FormLabel from '@mui/material/FormLabel';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import { id } from 'date-fns/locale';
+import FormGroup from '@mui/material/FormGroup';
+import Checkbox from '@mui/material/Checkbox';
 
 
 // - numer stolika (edytowalny)
@@ -53,13 +54,13 @@ const WaiterOrderNew = () => {
           options: {
             cucumber: {
               label: 'Cucumber',
-              price: 1,
+              price: 1, 
               default: true,
             },
             tomatoes: {
               label: 'Tomatoes',
               price: 1,
-              default: true,
+              default: true, 
             },
             olives: {
               label: 'Olives',
@@ -85,13 +86,141 @@ const WaiterOrderNew = () => {
             },
           },
         },
+        other_parameter: {
+          label: 'Custom parameter',
+          type: 'radios',
+          options: {
+            option1: {
+              label: 'option1',
+              price: 1,
+            },
+            option2: {
+              label: 'option2',
+              price: 1,
+              default: true,
+            },
+            option3: {
+              label: 'option3',
+              price: 1,
+            },
+          },
+        },
       },
     },
   ];
   
+  let selectedProduct = demoMenu[0];
+
+  // const getProduct = () => {
+  //   for (let product of demoMenu) {
+  //     const option = product.params.ingredients.options;
+  //     for (let optionLabel in option) {
+  //       return optionLabel;
+  //     }
+  //   }};
+
 
   // const [selected, setSelected] = React.useState(false);
 
+  /* 
+                  console
+                  1. Muszę mieć referecje do zaznaczonego produktu - Done: selectedProduct
+                  2. Muszę się dobrać do właściwości params wybranego produktu - done: electedProduct.param
+                  3. Musimy przejść pętlą po wszystkich parametrach (params)  - done
+                  4. Musimy mieć referecję do konkretnego parametru (w naszym przykładzie jest to ingriedients, other_parameter) - done
+                  5. Stwórz kontener wyświetlający formularz, wyświetl nazwę danego parametru - done
+                  6. Muszę się dobrać do właściwości options danego parametru - done 
+                  7. Musimy przejść pętlą po wszystkich opcjach (options) - done
+                  8. Musimy mieć referecję do konkretnej opcji (w naszym przykładzie jest to cucumber, tomatoes...) - done
+                  9. Wyświetl radio/checkbox dla danej opcji
+                  */
+
+  const renderParamsOfSelectedProduct = () => {
+    const renderedParamForms = [];
+    for (let paramKey in selectedProduct.params) {
+      const param = selectedProduct.params[paramKey];
+      renderedParamForms.push(renderParamForm(param));
+    }
+    return renderedParamForms;
+  };
+  
+  const renderParamForm = (param) => {
+    return (<Container>
+      <h2>{param.label}</h2>
+      {renderOptionsForParam(param)}
+    </Container>);
+  };
+
+  const renderOptionsForParam = (param) => {
+    if (param.type === 'radios') {
+      return renderRadioForm(param.options);
+    } else {
+      return renderCheckboxForm(param.options);
+    }
+  };
+
+  {/* <FormGroup>
+      <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+      <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
+    </FormGroup> */}
+
+
+  const renderRadioForm = (options) => {
+
+    let defaultOptionKey;
+
+    for (let optionKey in options) {
+      const option = options[optionKey];
+      
+      if (option.default) {
+        defaultOptionKey = optionKey;
+      }
+    }
+
+    return (
+      <FormControl component="fieldset">
+        
+        <RadioGroup
+          aria-label="gender"
+          defaultValue={defaultOptionKey}
+          name="radio-buttons-group"
+        >
+          {(() => {
+            const views = [];
+
+            for (let optionKey in options) {
+              const option = options[optionKey];
+              views.push((
+                <FormControlLabel value={optionKey} control={<Radio />} label={option.label} />
+              ));
+            }
+
+            return views;          
+          })()}
+        </RadioGroup>
+      </FormControl>
+    );
+  };
+
+  const renderCheckboxForm = (options) => {
+    console.log('run renderCheckboxForm');
+    return(
+      <FormGroup>
+        {(() => {
+          const views = [];
+
+          for (let optionKey in options) {
+            const option = options[optionKey];
+            views.push((
+              <FormControlLabel control={<Checkbox />} label={option.label} />
+            ));
+          }
+
+          return views;          
+        })()}
+      </FormGroup>
+    );
+  };
 
   return (
     <div className={styles.component}>
@@ -105,7 +234,7 @@ const WaiterOrderNew = () => {
                 <TableCell key={demoTables.id} align="right">New order</TableCell>
               </TableRow>
             </TableHead>
-            <TableBody>
+            <TableBody> 
               <TableRow>
                 <TableCell>Table number</TableCell>
                 <TableCell>
@@ -135,9 +264,15 @@ const WaiterOrderNew = () => {
                       value={demoMenu[0].id}
                       label="Menu"
                       onChange={(newValue) => {
-                        console.log(newValue);
+                        // console.log(newValue);
+                        selectedProduct = newValue;
                       }}
-                    >{demoMenu.map(product => (<MenuItem key={product.id} value={product.id}>{product.name}</MenuItem>))}
+                    >
+                      {
+                        demoMenu.map(product => (
+                          <MenuItem key={product.id} value={product.id}>{product.name}</MenuItem>
+                        ))
+                      }
                     </Select>
                   </FormControl>
                 </TableCell>
@@ -146,25 +281,15 @@ const WaiterOrderNew = () => {
               <TableRow>
                 <TableCell>Product</TableCell>
                 <TableCell>
-                  <FormControl component="fieldset">
-                    <RadioGroup
-                      aria-label="gender"
-                      defaultValue="female"
-                      name="radio-buttons-group"
-                    >
-                      {demoMenu.map(product => (<FormControlLabel key={product.id} value={product.params.ingredients.options[id].label} control={<Radio />} label={product.params.ingredients.options[id.label]}/>))}
-                      <FormControlLabel value="female" control={<Radio />} label="Female" />
-                      <FormControlLabel value="male" control={<Radio />} label="Male" />
-                      <FormControlLabel value="other" control={<Radio />} label="Other" />
-                    </RadioGroup>
-                  </FormControl>
+                  {console.log(selectedProduct.params)}                  
+                  {renderParamsOfSelectedProduct()}
                 </TableCell>
               </TableRow>
 
               <TableRow>
                 <TableCell>Product options</TableCell>
                 <TableCell>
-                  <FormControl fullWidth>
+                  {/* <FormControl fullWidth>
                     <InputLabel id="demo-simple-select-label">Menu</InputLabel>
                     <Select
                       labelId="Menu"
@@ -176,7 +301,7 @@ const WaiterOrderNew = () => {
                       }}
                     >{demoMenu.map(product => (<MenuItem key={product.id} value={product.id}>{product.name}</MenuItem>))}
                     </Select>
-                  </FormControl>
+                  </FormControl> */}
                 </TableCell>
               </TableRow>
 
